b7f35ea9bfb4f29d9d5078879ca7c5c8ebb223d3
lee <whddlsk123@naver.com>
2023-05-19 10:27:00 +0900

    fix : 2023-05-19
    에러코드 주석처리

--- src/main/java/com/example/sstv/streaming/UserTestController.java
+++ src/main/java/com/example/sstv/streaming/UserTestController.java
@@ -1,47 +1,47 @@
-package com.example.sstv.streaming;
-
-import com.example.sstv.common.Data;
-import com.example.sstv.common.NodeCookie;
-import com.example.sstv.user.User;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserTestController {
-
-
-    @GetMapping("/")
-    public Data test(HttpServletResponse response, HttpSession session) {
-        NodeCookie nodeCookie = new NodeCookie();
-
-        User user = new User();
-        user.setUserId("userId");
-        user.setPassword("pwd");
-
-        session.setAttribute("user", user);
-
-
-        Cookie cookie = nodeCookie.getNodeCookie(user);
-        response.addCookie(cookie);
-
-        Data data = new Data("success", user);
-
-        return data;
-    }
-
-    @GetMapping("/logout")
-    public String logout(@CookieValue("NSESSIONID") String cookieName, HttpServletResponse response) {
-        NodeCookie nodeCookie = new NodeCookie();
-
-        nodeCookie.removeNodeCookie(cookieName);
-        Cookie cookie = nodeCookie.removeSpringCookie(cookieName);
-
-        response.addCookie(cookie);
-
-        return "logout";
-    }
-}
+//package com.example.sstv.streaming;
+//
+//import com.example.sstv.common.Data;
+//import com.example.sstv.common.NodeCookie;
+//import com.example.sstv.user.User;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import org.springframework.web.bind.annotation.CookieValue;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class UserTestController {
+//
+//
+//    @GetMapping("/")
+//    public Data test(HttpServletResponse response, HttpSession session) {
+//        NodeCookie nodeCookie = new NodeCookie();
+//
+//        User user = new User();
+//        user.setUserId("userId");
+//        user.setPassword("pwd");
+//
+//        session.setAttribute("user", user);
+//
+//
+//        Cookie cookie = nodeCookie.getNodeCookie(user);
+//        response.addCookie(cookie);
+//
+//        Data data = new Data("success", user);
+//
+//        return data;
+//    }
+//
+//    @GetMapping("/logout")
+//    public String logout(@CookieValue("NSESSIONID") String cookieName, HttpServletResponse response) {
+//        NodeCookie nodeCookie = new NodeCookie();
+//
+//        nodeCookie.removeNodeCookie(cookieName);
+//        Cookie cookie = nodeCookie.removeSpringCookie(cookieName);
+//
+//        response.addCookie(cookie);
+//
+//        return "logout";
+//    }
+//}
